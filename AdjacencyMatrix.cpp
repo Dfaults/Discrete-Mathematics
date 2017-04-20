489ee48f68b3b0d3504eea4ae683018bf86a1d2d
Jean Luis <jfel7598@gmail.com>
2017-04-20 13:48:31 -0400

    Adjacency Calculation
    Started the project to calculate Adjacency Matrix. To see more
    information about the project refer to AdjacencyMatrix.cpp under the
    Description.

---
+++ AdjacencyMatrix.cpp
@@ -0,0 +1,22 @@
+/*
+
+Author: Dfaults
+Description: Write a program that randomly generates an n x n adjacency matrix. 
+			 Have your program print the adjacency matrix, the number of edges,
+			 and the degree in each vertex.
+
+*/
+
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+	/
+	return 0;
+}
